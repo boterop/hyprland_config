096d133fe939bd14504e82661783deb5d8dac6be
boterop <boterop22@gmail.com>
2026-05-18 00:19:13 -0500

    Fix install script

--- install.sh
+++ install.sh
@@ -9,9 +9,12 @@ mkdir -p ~/.config/hypr
 mkdir -p ~/.config/mako
 mkdir -p ~/.config/kitty
 
+echo "Cleaning cached files"
+rm -rf $DIR
+
 echo "Install dependencies"
 sudo -v
-pacman -S --noconfirm git \
+sudo pacman -S --noconfirm git \
   ttf-jetbrains-mono-nerd \
   mako \
   cliphist \
@@ -30,10 +33,10 @@ echo "Downloading config files"
 git clone https://github.com/boterop/hyprland_config $DIR
 
 echo "Copying config files"
-cp $DIR/hypr/* ~/.config/hypr/
-cp $DIR/mako/* ~/.config/mako/
-cp $DIR/kitty/* ~/.config/kitty/
-cp $DIR/starship.toml ~/.config/
+cp -r $DIR/hypr/* ~/.config/hypr/
+cp -r $DIR/mako/* ~/.config/mako/
+cp -r $DIR/kitty/* ~/.config/kitty/
+cp -r $DIR/starship.toml ~/.config/
 
 chmod +x ~/.config/hypr/scripts/*.sh
 
