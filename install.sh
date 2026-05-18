7252caabd4fdb5028ff60b70cdcf1cc18278f09b
boterop <boterop22@gmail.com>
2026-05-18 11:13:28 -0500

    Add wallpaper

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ echo "Creating config directories"
 mkdir -p ~/.config/hypr
 mkdir -p ~/.config/mako
 mkdir -p ~/.config/kitty
+mkdir -p ~/Pictures/Wallpapers
 
 echo "Cleaning cached files"
 rm -rf $DIR
@@ -31,7 +32,11 @@ sudo pacman -S --noconfirm git \
   rofimoji
 
 echo "Downloading config files"
-git clone https://github.com/boterop/hyprland_config $DIR
+if [ "$1" = "--local" ]; then
+  cp -r . $DIR
+else
+  git clone https://github.com/boterop/hyprland_config $DIR
+fi
 
 echo "Copying config files"
 cp -r $DIR/hypr/* ~/.config/hypr/
@@ -39,6 +44,7 @@ cp -r $DIR/mako/* ~/.config/mako/
 cp -r $DIR/kitty/* ~/.config/kitty/
 cp -r $DIR/starship.toml ~/.config/
 cp -r $DIR/rofimoji.rc ~/.config/
+cp -r $DIR/wallpapers/* ~/Pictures/Wallpapers/
 
 chmod +x ~/.config/hypr/scripts/*.sh
 
